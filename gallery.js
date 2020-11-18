9709a3a6c3522326bb1d878f282c3eeccef9b09d
Stanislava99 <stanislavam@live.co.uk>
2020-11-18 23:58:24 +0100

    changes

---
+++ gallery.js
@@ -0,0 +1,78 @@
+let count1=0;
+document.getElementById('likes-photo1').onclick=function(){
+    count1++;
+    document.getElementById('likes-photo1').innerHTML=count1;
+
+}
+
+let count2=0;
+
+document.getElementById('likes-photo2').onclick=function(){
+    count2++;
+    document.getElementById('likes-photo2').innerHTML=count2;
+
+}
+
+
+let count3=0;
+
+document.getElementById('likes-photo3').onclick=function(){
+    count3++;
+    document.getElementById('likes-photo3').innerHTML=count3;
+
+}
+
+
+let count4=0;
+
+document.getElementById('likes-photo4').onclick=function(){
+    count4++;
+    document.getElementById('likes-photo4').innerHTML=count4;
+
+}
+
+
+let count5=0;
+
+document.getElementById('likes-photo5').onclick=function(){
+    count5++;
+    document.getElementById('likes-photo5').innerHTML=count5;
+
+}
+
+
+let count6=0;
+
+document.getElementById('likes-photo6').onclick=function(){
+    count6++;
+    document.getElementById('likes-photo6').innerHTML=count6;
+
+}
+
+
+let count7=0;
+
+document.getElementById('likes-photo7').onclick=function(){
+    count7++;
+    document.getElementById('likes-photo7').innerHTML=count7;
+
+}
+
+
+let count8=0;
+
+document.getElementById('likes-photo8').onclick=function(){
+    count8++;
+    document.getElementById('likes-photo8').innerHTML=count8;
+
+}
+
+
+let count9=0;
+
+document.getElementById('likes-photo9').onclick=function(){
+    count9++;
+    document.getElementById('likes-photo9').innerHTML=count9;
+
+}
+
